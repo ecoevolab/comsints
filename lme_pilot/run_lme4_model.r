aae6666b91c64079582e3bbb05dc1d9c24734ae8
sur <surh@users.noreply.github.com>
2025-03-10 18:44:01 -0600

    Adding brms of negbinomial model

--- lme_pilot/run_lme4_model.r
+++ lme_pilot/run_lme4_model.r
@@ -42,6 +42,8 @@ m4.t1 <-  glmer(count ~ log(depth) + i_freq +
 AIC(m1.t1, m2.t1, m3.t1, m4.t1)
 BIC(m1.t1, m2.t1, m3.t1, m4.t1)
 
+#' Basically we need to include the overdispersion parameter.
+#' The model without interactions provides a better fit.
 #' Now fit the negative binomial
 
 m5.t1 <- glmer.nb(count ~ log(depth) + i_freq + 
@@ -55,9 +57,87 @@ m6.t1 <- glmer.nb(count ~ log(depth) + i_freq +
 
 
 AIC(m1.t1, m2.t1, m3.t1, m4.t1, m5.t1, m6.t1)
+# df        AIC
+# m1.t1 126   4288.263
+# m2.t1   7   4570.509
+# m3.t1 125  91657.828
+# m4.t1   6 240681.080
+# m5.t1 126   4277.931
+# m6.t1   7   4563.837
 BIC(m1.t1, m2.t1, m3.t1, m4.t1, m5.t1, m6.t1)
+# df        BIC
+# m1.t1 126   4724.171
+# m2.t1   7   4594.726
+# m3.t1 125  92090.277
+# m4.t1   6 240701.837
+# m5.t1 126   4713.839
+# m6.t1   7   4588.054
 
-save()
+save(m1.t1, m2.t1, m3.t1, m4.t1, m5.t1, m6.t1, file = "models.rdat")
+
+#' There were convergence issues but negative binomial performs well
+#' There is disagreement between AIC and BIC. BIC gives the models without
+#' temp by community interactions better scores while AIC gives the models with
+#' temperature x community interaction as better
+#' 
+#' From the winbugs manual:
+#' 
+#' 
+#' In hierarchical models, these three techniques are
+#' essentially answering different prediction problems.
+#' Suppose the three levels of our model concerned
+#' classes within schools within a country. Then
+#' 1. if we were interested in predicting results of
+#' future classes in those actual schools, then
+#' DIC is appropriate (ie the random effects
+#'                     themselves are of interest);
+#' 2. if we were interested in predicting results of
+#' future schools in that country, then marginal-
+#'   likelihood methods such as AIC are
+#' appropriate (ie the population parameters are
+#'              of interest);
+#' 3. if we were interested in predicting results for
+#' a new country, then BIC/ Bayes factors are
+#' appropriate (ie the 'true' underlying model is
+#'              of interest).
+
+#' We are interested in future run of these communities, thus
+#' DIC may be more relevant?
+
+
+#' Will run brms with the negbinomial model
+library(brms)
+
+
+m5.br.t1 <- brm(count ~ log(depth) + i_freq + 
+                  (1|b_com) + (1|b_temp) + (b_temp_com) + (1|b_rep),
+                data = dat,
+                chains = 4,
+                iter = 4000,
+                warmup = 1000,
+                cores = 4,
+                thin = 2,
+                family = "negbinomial")
+
+
+m6.br.t1 <- brm(count ~ log(depth) + i_freq + 
+                  (1|b_com) + (1|b_temp) + (1|b_rep),
+                data = dat,
+                chains = 4,
+                iter = 4000,
+                warmup = 1000,
+                cores = 4,
+                thin = 2,
+                family = "negbinomial")
+
+
+summary(m6.br.t1)
+summary(m6.t1)
+
+
+brms::waic(m5.br.t1, m6.br.t1)
+
+
 
 summary(m1.t1)
 
